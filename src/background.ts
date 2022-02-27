e706c523b85f4dc84f19263deb0cffef23978489
Hideaki Tanabe <tanablog@gmail.com>
2022-02-27 16:23:27 +0900

    Copy URL

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,26 @@
 import { browser } from 'webextension-polyfill-ts'
 
 const execute = async () => {
-  alert("hello world");
+  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    let url = tabs[0].url;
+    if (url != null) {
+      copyToClipboard(url);
+      alert("hello world");
+    } else {
+      alert(url);
+    }
+  });
 }
+
+const copyToClipboard = (text: string) => {
+  var input = document.createElement('input');
+  input.setAttribute('id', 'copyinput');
+  document.body.appendChild(input);
+  input.value = text;
+  input.select();
+  document.execCommand('copy');
+  document.body.removeChild(input);
+};
 
 browser.commands.onCommand.addListener((command) => {
   execute();
