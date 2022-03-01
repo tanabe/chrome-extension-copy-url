7fe6de29d482e4b9cdcf946e69d9bbbaab430106
Hideaki Tanabe <tanablog@gmail.com>
2022-03-01 23:30:32 +0900

    WIP Option page

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,58 @@
 import { browser } from 'webextension-polyfill-ts'
 
+interface UrlItem {
+  url: string,
+  matcher: string
+}
+
+interface Preference {
+  items: [UrlItem],
+  version: string
+}
+
+// sample preferences
+const savePreferences = async () => {
+  let preference: Preference = {
+    items: [
+      {url: "https://github.com", matcher: "https://github.com/([^/]+/[^/]+)"}
+    ],
+    version: '0.1.0'
+  };
+  browser.storage.sync.set({'preference': JSON.stringify(preference)}).then(() => {
+    // do nothing
+  });
+}
+
+const loadPreference = async (): Promise<Preference> => {
+  return browser.storage.sync.get('preference').then((json) => {
+    let result = JSON.parse(json.preference);
+    return result;
+  });
+}
+
 const execute = async () => {
+  let preference = await loadPreference();
+  if (preference == null) {
+    return;
+  }
+
   browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
     let url = tabs[0].url;
     if (url != null) {
-      copyToClipboard(url);
-      alert("copied!");
+      preference.items.forEach((item) => {
+        if (url!.indexOf(item.url) > -1) {
+          let regexp = new RegExp(item.matcher);
+          let matches = url?.match(regexp);
+          console.log(item.matcher);
+          if (matches) {
+            let parialUrl = matches[1];
+            alert(`copied ${parialUrl}`);
+            copyToClipboard(parialUrl);
+          }
+        }
+      });
     } else {
-      alert(url);
+      alert('failed');
     }
   });
 }
@@ -21,6 +66,10 @@ const copyToClipboard = (text: string) => {
   document.execCommand('copy');
   document.body.removeChild(input);
 };
+
+browser.runtime.onInstalled.addListener(() => {
+  savePreferences()
+});
 
 browser.commands.onCommand.addListener((command) => {
   execute();
